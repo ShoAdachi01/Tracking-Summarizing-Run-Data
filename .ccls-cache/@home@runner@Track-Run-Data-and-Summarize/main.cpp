104a77b9e6b8d9dac26bfd24f294cc4fab140639
Sho Adachi <20816065-ShoAdachi@users.noreply.replit.com>
2024-05-07 01:14:25 +0000

    Initial commit

---
+++ .ccls-cache/@home@runner@Track-Run-Data-and-Summarize/main.cpp
@@ -0,0 +1,13 @@
+/*
+Programmed by Sho Adachi
+Programmed for ICS3U
+Programmed on June 13th
+  */
+#include "functions.h"
+#include <iostream>
+using namespace std;
+
+int main() {
+  logInMenu();
+  return 0;
+}
---
+++ .ccls-cache/@home@runner@TrackRunData/main.cpp
@@ -0,0 +1,8 @@
+#include "functions.h"
+#include <iostream>
+using namespace std;
+
+int main() {
+  logInMenu();
+  return 0;
+}
